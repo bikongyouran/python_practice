951680bed69c431c1c9863bbad8a43971ec6bb9a
bikongyouran <jasonlin16@gmail.com>
2016-06-13 15:53:29 +0800

    add more implementations.

--- MachineLearningPractice/SVM_face_recongnition.py
+++ MachineLearningPractice/SVM_face_recongnition.py
@@ -79,6 +79,56 @@ print (clf.best_estimator_)
 # Quantitative evaluation of the model quality on the test set
 
 print ("Predicting people's names on the test set")
+t0 = time()
+y_pred = clf.predict(X_test_pca)
+print ('done in %0.3fs' % (time() -t0))
+
+print (classification_report(y_test, y_pred, target_names=target_names))
+print (confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+###################################################
+# Quantitative evaluation of the predictions using matplotlib
+def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+    '''Helper function to plot a gallery of portraits'''
+    plt.figure(figsize=(1.8*n_col, 2.4*n_row))
+    plt.subplots_adjust(bottom=0,left=.01,right=.99,top=.90,hspace=.35)
+    for i in range(n_row*n_col):
+        plt.subplot(n_row,n_col,i+1)
+        plt.imshow(images[i].reshape((h,w)), cmap=plt.cm.gray)
+        plt.title(titles[i],size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+
+#plot the result of the prediction on a portion of the test set
+def title(y_pred,y_test,target_names,i):
+    pred_name = target_names[y_pred[i]].rsplit(' ',1)[-1]
+    true_name = target_names[y_test[i]].rsplit(' ',1)[-1]
+    return 'predicted: %s\ntrue: %s' % (pred_name, true_name)
+
+prediction_titles = [title(y_pred,y_test,target_names,i) for i in range(y_pred.shape[0])]
+
+plot_gallery(X_test, prediction_titles,h,w)
+
+#plot the gallery of the most significative eigenfaces
+
+eigenfaces_titles = ['eigenface %d' % i for i in range(eigenfaces.shape[0])]
+plot_gallery(eigenfaces,eigenfaces_titles,h,w)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
