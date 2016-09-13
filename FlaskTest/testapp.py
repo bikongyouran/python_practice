0cb1b2e9ea242bf875bb6eee9f186b44e4c537e1
bikongyouran <jasonlin16@gmail.com>
2016-09-13 17:49:55 +0800

    add test flask app

--- FlaskTest/testapp.py
+++ FlaskTest/testapp.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import render_template
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello():
     return 'hello world!'
+
+@app.route('/user/<name>')
+def user(name):
+    return 'hello, %s!' % name
+
+@app.route('/users')
+def user2():
+    return render_template('user.html', name = 'aaa')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
