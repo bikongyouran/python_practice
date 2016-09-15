fd15dc2568adf3a860ed5b19638a5d20b738b02d
bikongyouran <jasonlin16@gmail.com>
2016-09-16 01:57:24 +0800

    update

--- FlaskTest/testapp.py
+++ FlaskTest/testapp.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_bootstrap import Bootstrap
-from flask import render_template
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+#prevent csrf. this config should be written in config file.
+app.config['SECRET_KEY'] = 'hard to guess string'
 Bootstrap(app)
 
-@app.route('/')
-def hello():
-    return 'hello world!'
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('user.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
