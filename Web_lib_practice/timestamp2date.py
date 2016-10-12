fc2710c0cd2f2746db3b535c31638b2819b8f072
bikongyouran <jasonlin16@gmail.com>
2016-10-12 17:47:41 +0800

    update

--- Web_lib_practice/timestamp2date.py
+++ Web_lib_practice/timestamp2date.py
@@ -3,8 +3,12 @@ import pytz
 from datetime import datetime
 
 #PST timestamp to date
-timestamp = 1153267200
+timestamp = 1476259617
 tz = pytz.timezone('US/Pacific')
-dateArray = datetime.fromtimestamp(int(timestamp),tz)
+dateArray=0
+if len(str(timestamp)) > 11:
+    dateArray = datetime.fromtimestamp(int(timestamp) / 1000, tz)
+else:
+    dateArray = datetime.fromtimestamp(int(timestamp),tz)
 real_time = dateArray.strftime("%Y-%m-%d %H:%M:%S")
 print real_time;
