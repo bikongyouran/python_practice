ec8ae1df2e506b70a922d4bc83b7bae1566b432b
bikongyouran <jasonlin16@gmail.com>
2016-10-28 17:51:30 +0800

    update more graph layout.

--- spark/testJob.py
+++ spark/testJob.py
@@ -23,20 +23,59 @@
 
 ##################################################################
 '''if pycharm is configured, the typical sample would be:'''
-from pyspark import SparkContext
+# from pyspark import SparkContext
+#
+# sc = SparkContext('local')
+# #There are two ways to create RDDs:
+# # parallelizing an existing collection in your driver program,
+# # or referencing a dataset in an external storage system
+# textFile = sc.textFile("C:\codes\python_practice\spark\README.md")
+# count = textFile.count()
+# print count
+# linesWithSpark = textFile.filter(lambda line: "Spark" in line)
+# count2 = linesWithSpark.count()
+# print count2
+# count3 = textFile.map(lambda line: len(line.split())).reduce(lambda a, b: a if (a > b) else b)
+# print count3
+#
+# # words = sc.parallelize(["scala","java","hadoop","spark","akka"])
+# # print(words.count())
 
-sc = SparkContext('local')
-#There are two ways to create RDDs:
-# parallelizing an existing collection in your driver program,
-# or referencing a dataset in an external storage system
-textFile = sc.textFile("C:\codes\python_practice\spark\README.md")
-count = textFile.count()
-print count
-linesWithSpark = textFile.filter(lambda line: "Spark" in line)
-count2 = linesWithSpark.count()
-print count2
-count3 = textFile.map(lambda line: len(line.split())).reduce(lambda a, b: a if (a > b) else b)
-print count3
+##################################################################
+'''spark sql sample.'''
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql import Row
 
-# words = sc.parallelize(["scala","java","hadoop","spark","akka"])
-# print(words.count())
+spark = SparkSession.builder.appName('Python Spark SQL sample').config("spark.some.config.option", "some-value").getOrCreate()
+sc = spark.sparkContext
+
+# Load a text file and convert each line to a Row.
+lines = sc.textFile("C:\mySoftware\spark-2.0.1-bin-hadoop2.7\examples\src\main\\resources\people.txt")
+parts = lines.map(lambda l: l.split(","))
+# Each line is converted to a tuple.
+people = parts.map(lambda p: (p[0], p[1].strip()))
+
+# The schema is encoded in a string.
+schemaString = "name age"
+
+fields = [StructField(field_name, StringType(), True) for field_name in schemaString.split()]
+schema = StructType(fields)
+
+# Apply the schema to the RDD.
+schemaPeople = spark.createDataFrame(people, schema)
+
+# Creates a temporary view using the DataFrame
+schemaPeople.createOrReplaceTempView("people")
+
+# Creates a temporary view using the DataFrame
+schemaPeople.createOrReplaceTempView("people")
+
+# SQL can be run over DataFrames that have been registered as a table.
+results = spark.sql("SELECT name FROM people")
+
+results.show()
+
+spark.stop()
+
+##################################################################
