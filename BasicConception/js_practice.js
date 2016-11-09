c0ad78fa9bacc391a339b101fed6012c515f246f
bikongyouran <jasonlin16@gmail.com>
2016-11-09 15:25:37 +0800

    update

--- BasicConception/js_practice.js
+++ BasicConception/js_practice.js
@@ -1,41 +1,41 @@
-function add(a,b){
+function add(a, b) {
     return a + b;
 }
 
-function mul(a,b){
-    return a*b;
+function mul(a, b) {
+    return a * b;
 }
 
-function sub(a,b){
-    return a-b;
+function sub(a, b) {
+    return a - b;
 }
 
 function identify(x) {
-  return function(){
-    return x;
-  };
+    return function() {
+        return x;
+    };
 }
 
 var three = identify(3);
 
 
-function addf(x){
-  return function(y){
-    return x + y;
-  };
+function addf(x) {
+    return function(y) {
+        return x + y;
+    };
 }
 
-function curry(binary,x){
-  return function(y){
-    return binary(x,y);
-  };
+function curry(binary, x) {
+    return function(y) {
+        return binary(x, y);
+    };
 }
 //var add3 = curry(add,3);
 
-function curryr(binary, x){
-  return function(y){
-    return binary(y,x);
-  }
+function curryr(binary, x) {
+    return function(y) {
+        return binary(y, x);
+    }
 }
 var sub3 = curryr(sub, 3);
 //alert(sub3(11))
@@ -48,12 +48,12 @@ var sub3 = curryr(sub, 3);
 //}
 
 //second
-function liftf(binary){
-  return function(x){
-    return function(y){
-      return binary(x,y)
+function liftf(binary) {
+    return function(x) {
+        return function(y) {
+            return binary(x, y)
+        }
     }
-  }
 }
 
 var addf = liftf(add);
@@ -62,13 +62,13 @@ var addf = liftf(add);
 //var inc = liftf(add)(1)
 //var inc = addf(1)
 //var inc = curry(add,1)
-var inc = curryr(add,1)
-//alert(inc(5))
+var inc = curryr(add, 1)
+    //alert(inc(5))
 
-function twice(binary){
-  return function(x){
-    return binary(x,x)
-  }
+function twice(binary) {
+    return function(x) {
+        return binary(x, x)
+    }
 }
 
 var double = twice(add);
@@ -76,10 +76,10 @@ var double = twice(add);
 var square = twice(mul);
 //alert(square(11));
 
-function reverse(binary){
- return function(x,y) {
-   return binary(y,x);
- }
+function reverse(binary) {
+    return function(x, y) {
+        return binary(y, x);
+    }
 }
 
 // function reverse(binary){
@@ -90,54 +90,54 @@ function reverse(binary){
 var bus = reverse(sub);
 //alert(bus(3,2))
 
-function composeu(func1, func2){
-  return function(x){
-    return func2(func1(x));
-  }
+function composeu(func1, func2) {
+    return function(x) {
+        return func2(func1(x));
+    }
 }
 //alert(composeu(double,square)(5));
 
-function composeb(func1, func2){
-  return function(x, y, z){
-    return func2(func1(x, y), z);
-  }
+function composeb(func1, func2) {
+    return function(x, y, z) {
+        return func2(func1(x, y), z);
+    }
 }
 //alert(composeb(add,mul)(2,3,7));
 
-function limit(func, n){
-  temp = 0;
-  return function(x,y){
-    if (temp < n) {
-      temp += 1;
-      return func(x,y);
+function limit(func, n) {
+    temp = 0;
+    return function(x, y) {
+        if (temp < n) {
+            temp += 1;
+            return func(x, y);
+        }
+        return undefined
     }
-    return undefined
-  }
 }
 var add_ltd = limit(add, 1)
-//alert(add_ltd(3,4))
-//alert(add_ltd(3,5))
-//alert(add_ltd(3,6))
+    //alert(add_ltd(3,4))
+    //alert(add_ltd(3,5))
+    //alert(add_ltd(3,6))
 
-function from(n){
-  return function(){
-    var temp = n
-    n += 1
-    return temp
-  }
+function from(n) {
+    return function() {
+        var temp = n
+        n += 1
+        return temp
+    }
 }
 //var gen = from(0);
 //alert(gen());
 //alert(gen());
 //alert(gen());
 
-function to(func,n){
-  return function(){
-    var temp = func()
-    if (temp < n){
-      return temp
+function to(func, n) {
+    return function() {
+        var temp = func()
+        if (temp < n) {
+            return temp
+        }
     }
-  }
 }
 //var gen = to(from(3),5)
 //alert(gen());
@@ -154,37 +154,37 @@ function to(func,n){
 //  }
 //}
 
-function fromTo(a, b){
-  return to(from(a),b);
+function fromTo(a, b) {
+    return to(from(a), b);
 }
 //var gen = fromTo(0,3);
 //alert(gen());
 //alert(gen());
 //alert(gen());
 
-function element(array,func){
-  return function(){
-    var index = func();
-    if(index !== undefined){
-      return array[index];
+function element(array, func) {
+    return function() {
+        var index = func();
+        if (index !== undefined) {
+            return array[index];
+        }
     }
-  }
 }
 //var gen = element(['a','b','c','d'],fromTo(1,3))
 //alert(gen());
 //alert(gen());
 //alert(gen());
 
-function element(array,func){
-  if (func === undefined){
-    func = fromTo(0, array.length);
-  }
-  return function(){
-    var index = func();
-    if(index !== undefined){
-      return array[index];
+function element(array, func) {
+    if (func === undefined) {
+        func = fromTo(0, array.length);
     }
-  }
+    return function() {
+        var index = func();
+        if (index !== undefined) {
+            return array[index];
+        }
+    }
 }
 //var gen = element(['a','b','c','d'])
 //alert(gen());
@@ -193,14 +193,14 @@ function element(array,func){
 //alert(gen());
 //alert(gen());
 
-function collect(func, array){
-  return function(){
-    var temp = func()
-    if (temp !== undefined){
-      array.push(temp)
+function collect(func, array) {
+    return function() {
+        var temp = func()
+        if (temp !== undefined) {
+            array.push(temp)
+        }
+        return temp;
     }
-    return temp;
-  }
 }
 // var array =[]
 // var gen = collect(fromTo(0,2),array);
@@ -210,28 +210,28 @@ function collect(func, array){
 // console.log(array)
 
 //TODO
-function filter(func1,func2){
-  return function recur(){
-    var temp = func1();
-    if (func2(temp)===true) {
-      return temp;
+function filter(func1, func2) {
+    return function recur() {
+        var temp = func1();
+        if (func2(temp) === true) {
+            return temp;
+        }
     }
-  }
 }
 // var gen = filter(fromTo(1,5), function third(value){return (value % 3) === 0;});
 // console.log(gen());
 // console.log(gen());
 // console.log(gen());
 
-function concat(func1, func2){
-  return function(){
-    var temp1 = func1()
-    if (temp1 !== undefined){
-      return temp1;
+function concat(func1, func2) {
+    return function() {
+        var temp1 = func1()
+        if (temp1 !== undefined) {
+            return temp1;
+        }
+        var temp2 = func2();
+        return temp2;
     }
-    var temp2 = func2();
-    return temp2;
-  }
 }
 // var gen = concat(fromTo(0,3),fromTo(0,2));
 // console.log(gen());
@@ -241,11 +241,11 @@ function concat(func1, func2){
 // console.log(gen());
 // console.log(gen());
 
-function gensymf(prefix){
-  var temp = from(1);
-  return function(){
-    return prefix + temp();
-  }
+function gensymf(prefix) {
+    var temp = from(1);
+    return function() {
+        return prefix + temp();
+    }
 }
 // var geng =gensymf('G');
 // console.log(geng());
@@ -254,24 +254,24 @@ function gensymf(prefix){
 // console.log(genh());
 // console.log(genh());
 
-function fibonaccif(a,b){
-  var t1 = a;
-  var t2 = b;
-  var index = 0;
-  return function(){
-    if (index == 0){
-      index += 1
-      return t1
+function fibonaccif(a, b) {
+    var t1 = a;
+    var t2 = b;
+    var index = 0;
+    return function() {
+        if (index == 0) {
+            index += 1
+            return t1
+        }
+        if (index == 1) {
+            index += 1
+            return t2
+        }
+        var t3 = t1 + t2;
+        t1 = t2;
+        t2 = t3;
+        return t3;
     }
-    if (index == 1){
-      index += 1
-      return t2
-    }
-    var t3 = t1 + t2;
-    t1 = t2;
-    t2 = t3;
-    return t3;
-  }
 }
 // var fib = fibonaccif(0,1);
 // console.log(fib());
@@ -281,19 +281,19 @@ function fibonaccif(a,b){
 // console.log(fib());
 // console.log(fib());
 
-function counter(){
-  var temp = 0;
-  return {
-    up : function(){
-    temp += 1;
-    return temp;
-  },
+function counter() {
+    var temp = 0;
+    return {
+        up: function() {
+            temp += 1;
+            return temp;
+        },
 
-  down : function(){
-    temp -= 1;
-    return temp;
-  }
-  };
+        down: function() {
+            temp -= 1;
+            return temp;
+        }
+    };
 }
 // var object = counter();
 // var up = object.up;
@@ -303,20 +303,29 @@ function counter(){
 // console.log(down())
 // console.log(up())
 
-function revocable(f){
-  return {
-    invoke: function(x,y) {
-      if (f !== undefined){
-        return f(x,y);
-      }
-    },
-    revoke: function(){
-      f = undefined;
+function revocable(f) {
+    return {
+        invoke: function(x, y) {
+            if (f !== undefined) {
+                return f(x, y);
+            }
+        },
+        revoke: function() {
+            f = undefined;
+        }
     }
-  }
 }
 // var rev = revocable(add);
 // var add_rev = rev.invoke;
 // console.log(add_rev(3,4));
 // rev.revoke();
 // console.log(add_rev(3,7));
+
+function continuize(f){
+    return function(callback,a){
+        return callback(f(a));
+    }
+}
+sqrtc = continuize(Math.sqrt);
+sqrtc(console.log,81);
+
