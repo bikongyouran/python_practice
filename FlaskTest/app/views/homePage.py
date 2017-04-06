f8d2d13072ef91efd1c592cfdadbe6d0c72eb868
bikongyouran <jasonlin16@gmail.com>
2017-04-06 18:08:40 +0800

    adapt python3 and add d3.

--- FlaskTest/app/views/homePage.py
+++ FlaskTest/app/views/homePage.py
@@ -1,17 +1,17 @@
 from flask import render_template,session,redirect,url_for,flash
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from . import myApp
-from ..models import User
-from .. import db
-
-@myApp.route('/user/<name>')
-def user(name):
-    testUser = User(name, name + '@test.com')
-    db.session.add(testUser)
-    db.session.commit()
-    return 'hello, %s!' % name
+# from ..models import User
+# from .. import db
+#
+# @myApp.route('/user/<name>')
+# def user(name):
+#     testUser = User(name, name + '@test.com')
+#     db.session.add(testUser)
+#     db.session.commit()
+#     return 'hello, %s!' % name
 
 @myApp.route('/', methods=['GET','POST'])
 def index():
@@ -24,6 +24,10 @@ def index():
         return redirect(url_for('.index'))
     return render_template('user.html', form=form, name=session.get('name'))
 
-class NameForm(Form):
+@myApp.route('/d3', methods=['GET','POST'])
+def d3_test():
+    return render_template('d3.html')
+
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
