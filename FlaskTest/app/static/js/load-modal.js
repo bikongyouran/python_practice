664e2d81fbd34daa2f3d70140c4d19d0335eaff6
bikongyouran <jasonlin16@gmail.com>
2017-09-17 15:44:27 +0800

    update

---
+++ FlaskTest/app/static/js/load-modal.js
@@ -0,0 +1,33 @@
+$(function () {
+//$('#myModal').on('shown.bs.modal', function() {
+//    alert('shown');
+//}
+
+    $("#myModal").modal("show");
+    $('#myModal').on('shown.bs.modal', function() {
+           var myChart2 = Highcharts.chart('container2', {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Fruit Consumption'
+            },
+            xAxis: {
+                categories: ['Apples', 'Bananas', 'Oranges']
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            series: [{
+                name: 'Jane',
+                data: [1, 0, 4]
+            }, {
+                name: 'John',
+                data: [5, 7, 3]
+            }]
+        });
+    })
+});
+
